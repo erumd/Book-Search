0516b156fef1f15366077e7d9ba30880cb57aab2
Erum Dhukka <erumdhukka531@gmail.com>
2021-05-08 20:26:46 -0500

    saved buttom is not working

--- client/src/components/card/BookCard.js
+++ client/src/components/card/BookCard.js
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import axios from "axios";
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const BookCard = ({ title, authors, image, description, googleLink }) => {
   const [newBook] = useState({
     title: title,
     authors: authors,
-    image: image,
     description: description,
+    image: image,
     googleLink: googleLink,
   });
-// save function
+  // save function
   const handleSave = (e) => {
-    axios.post("api/books", newBook).then((res) => {
-      console.log(res);
-      window.location.reload(false);
-    }).catch((err) => console.log(err));
+    axios
+      .post('/api/books', newBook)
+      .then((res) => {
+        console.log(res);
+        // window.location.reload(false);
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div className="container border">
       <div className="row">
         <div className="col-12">
           <h3 className="float-left">{title}</h3>
-         
-          <a href={googleLink} target="_blank" rel="noreferrer">
-            
-          </a>
+
+          <a href={googleLink} target="_blank" rel="noreferrer"></a>
         </div>
       </div>
       <div className="row">
@@ -35,19 +35,22 @@ const BookCard = ({ title, authors, image, description, googleLink }) => {
       </div>
       <div className="row">
         <div className="col-2">
-          <img style={{"maxWidth": "100%"}}src={image} alt={title} />
+          <img style={{ maxWidth: '100%' }} src={image} alt={title} />
         </div>
         <div className="col-10">
           <p>{description}</p>
         </div>
         <button type="button" className="btn btn-primary float-right">
-              View
-            </button>
-        <button type="button" onClick={handleSave} className="btn btn-success float-right">
-            Save
-          </button>
+          View
+        </button>
+        <button
+          type="button"
+          onClick={handleSave}
+          className="btn btn-success float-right"
+        >
+          Save
+        </button>
       </div>
-      
     </div>
   );
 };
